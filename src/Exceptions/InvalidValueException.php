e26dec478a753235d10eb2dbbb809713978a386f
tzsk <tzsk@users.noreply.github.com>
2021-09-26 08:28:42 +0000

    Apply php-cs Fixes

--- src/Exceptions/InvalidValueException.php
+++ src/Exceptions/InvalidValueException.php
@@ -9,16 +9,19 @@ class InvalidValueException extends Exception
 {
     public ?ValidationException $validationException;
 
-    public function __construct(string $message = 'Invalid Value', ?ValidationException $ex = null) {
+    public function __construct(string $message = 'Invalid Value', ?ValidationException $ex = null)
+    {
         parent::__construct($message, 400, $ex);
         $this->validationException = $ex;
     }
 
-    public static function fromValidationException(ValidationException $ex) {
+    public static function fromValidationException(ValidationException $ex)
+    {
         return new self($ex->validator->errors()->first(), $ex);
     }
 
-    public static function fromMessage(string $message, string $field) {
+    public static function fromMessage(string $message, string $field)
+    {
         return new self($message, ValidationException::withMessages([$field => $message]));
     }
 }
